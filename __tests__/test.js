13c4e71c12f08bb3247d2bdb9d2b5c3c72786a83
Max Schoening <max@max.dev>
2019-08-09 22:30:30 -0700

    feat: Add more exclamation

--- __tests__/test.js
+++ __tests__/test.js
@@ -2,6 +2,6 @@ const greet = require('../index')
 
 describe('greet', () => {
   test('greets a user with a given name', () => {
-    expect(greet('John Doe')).toEqual('Hello, John Doe')
+    expect(greet('John Doe')).toEqual('Hello, John Doe!')
   })
 })
